f8a0ce59516d34bd78bac42be0a28c628b556f3c
khjin <hyungjin941@gmail.com>
2024-06-20 19:08:24 +0900

    Update 문자열 내 p와 y의 갯수.java

--- 김형진/문자열 내 p와 y의 갯수.java
+++ 김형진/문자열 내 p와 y의 갯수.java
@@ -1,31 +1,30 @@
 
-import java.util.Scanner;
-
-class Solution_1 {
-    public static void main(String[] args) {
-
-
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-
-        str = str.toLowerCase();
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        
+        s = s.toLowerCase();
 
         int pCnt = 0;
         int yCnt = 0;
 
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == 'p')
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'p')
                 pCnt++;
-            if (str.charAt(i) == 'y')
+            if (s.charAt(i) == 'y')
                 yCnt++;
-            }
-            if (pCnt == yCnt) {
-                System.out.println("true");
-            }
-            else{
-                    System.out.println("false");
-                }
-            }
         }
+        if (pCnt == yCnt) {
+            
+            answer = true; 
+            return answer;
+        }
+        else{   
+            answer = false;
+            return answer;
+            
+        }
+    }
+}
 
 
