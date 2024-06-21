4533bb7cf1a74e88048a881f9c74a7408642f905
chiwonson <166478273+chiwonson@users.noreply.github.com>
2024-06-21 09:27:46 +0900

    Update 몫 구하기.java

--- 손치원/몫 구하기.java
+++ 손치원/몫 구하기.java
@@ -12,7 +12,7 @@ public class s_0620_01 {
         class Solution {
             public int solution(int num1, int num2) {
                 int answer = 0;
-                if ((-50000 <= num1 && num1 <= 50000) && (-50000 <= num2 && num2 <= 50000)) {
+                if ((0 < num1 && num1 <= 100) && (0 < num2 && num2 <= 100)) {
                     answer = num1 / num2;
                 }
                 return answer;
