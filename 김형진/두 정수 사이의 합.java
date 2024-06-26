5c4528ce4405e787dc8158a93d852eb84f937d8f
khjin <hyungjin941@gmail.com>
2024-06-26 23:55:57 +0900

    Update 두 정수 사이의 합.java

--- 김형진/두 정수 사이의 합.java
+++ 김형진/두 정수 사이의 합.java
@@ -1,1 +1,14 @@
-class solution
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        if(a<=b){
+            for (int i=a; i<=b; i++)
+                answer += i;
+        }
+        else {
+            for (int i =b; i<=a; i++)
+                answer += i;
+        }
+        return answer;
+    }
+}
