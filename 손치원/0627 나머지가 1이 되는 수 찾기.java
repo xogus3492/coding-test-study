d200e4dedfedc8c392bce9304d538ef274dd92f0
chiwonson <166478273+chiwonson@users.noreply.github.com>
2024-06-26 19:40:51 +0900

    Create 0627 나머지가 1 이 되는 수 찾기.java

---
+++ 손치원/0627 나머지가 1이 되는 수 찾기.java
@@ -0,0 +1,19 @@
+/*
+문제 설명
+    자연수 n이 매개변수로 주어집니다.
+    n을 x로 나눈 나머지가 1이 되도록 하는
+    가장 작은 자연수 x를 return 하도록 solution 함수를 완성해주세요.
+    답이 항상 존재함은 증명될 수 있습니다.
+*/
+class Solution {
+        public int solution(int n) {
+            int answer = 0;
+            for(int i=1; i<n; i++){
+                if(n % i == 1){     // if 문을 사용하여 n을 i로 나눈 나머지가 1인지 확인합니다.
+                    answer = i;     // % 연산자는 나머지를 구하는 연산자입니다.
+                    break;
+                }
+            }
+            return answer;
+        }
+    }
