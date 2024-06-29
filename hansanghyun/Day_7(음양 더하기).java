fbab6c9d4da6f7dd796c0bb35cbb93036551c5dc
sanghyun <135605716+sanghyunhann@users.noreply.github.com>
2024-06-29 23:40:55 +0900

    Create Day_7(음양 더하기).java

---
+++ hansanghyun/Day_7(음양 더하기).java
@@ -0,0 +1,31 @@
+//어떤 정수들이 있습니다. 이 정수들의 절댓값을 차례대로 담은 정수 배열 absolutes와 이 정수들의 부호를 차례대로 담은 불리언 배열 signs가 매개변수로 주어집니다. 실제 정수들의 합을 구하여 return 하도록 solution 함수를 완성해주세요.
+//
+//제한사항
+//absolutes의 길이는 1 이상 1,000 이하입니다.
+//absolutes의 모든 수는 각각 1 이상 1,000 이하입니다.
+//signs의 길이는 absolutes의 길이와 같습니다.
+//        signs[i] 가 참이면 absolutes[i] 의 실제 정수가 양수임을, 그렇지 않으면 음수임을 의미합니다.
+
+class Solution15 {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        int arr [] = new int[absolutes.length];
+
+        if (absolutes.length>=1 && absolutes.length<=1000 && absolutes.length == signs.length) {
+            for (int i=0; i<absolutes.length; i++) {
+                if (absolutes[i]>=1 && absolutes[i]<=1000) {
+                    if (signs[i] == true) {
+                        arr[i] = absolutes[i];
+                    } else {
+                        arr[i] = absolutes[i] - 2*absolutes[i];
+                    }
+                }
+            }
+         }
+
+        for (int i=0; i<arr.length; i++) {
+            answer += arr[i];
+        }
+        return answer;
+    }
+}
